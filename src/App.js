a7ced44888d3a08db5f058e00e49cd5f0e6a0816
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-02-28 13:30:41 +0530

    react router added to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import StartScreen from './Components/StartScreen'
 import MainScreen from './Components/MainScreen'
 import './App.css';
 
 function App() {
 
-  const [gameStart, setGameStart] = React.useState(false)
+  // const [gameStart, setGameStart] = React.useState(false)
 
-  function gameStarter() {
-    setGameStart(prevState => !prevState)
-  }
-
+  // function gameStarter() {
+  //   setGameStart(prevState => !prevState)
+  // }
   return (
     <div className="App">
-      {
+    <Routes>
+      <Route path='/' element={<StartScreen />} />
+      <Route path='/main' element={<MainScreen />} />
+    </Routes>
+
+      {/* {
         gameStart ?
           <MainScreen /> :
           <StartScreen gameStarter={gameStarter} />
-      }
+      } */}
     </div>
   );
 }
--- src/Components/StartScreen.js
+++ src/Components/StartScreen.js
@@ -1,12 +1,14 @@
 import './StartScreen.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function StartScreen(props) {
+    const navigation = useNavigate()
 
     return (
         <div className="start-screen">
             <h1 className="heading">Quizzical</h1>
             <p className="subheading">Let's answer some questions</p>
-            <button className="button" onClick={props.gameStarter}>Start quiz</button>
+            <button className="button" onClick={() => navigation('main')}>Start quiz</button>
         </div>
     )
 }
