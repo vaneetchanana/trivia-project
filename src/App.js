b3e59f0860e922c166130fdb26aa872632191279
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-04-24 15:34:34 +0530

    went back to browser router

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@
 import React from 'react';
 import {
   Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
+  Routes,
+  BrowserRouter,
 } from 'react-router-dom';
 import StartScreen from './Components/StartScreen'
 import MainScreen from './Components/MainScreen'
 import './App.css';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' >
-    <Route index element={<StartScreen />} />
-    <Route path='main' element={<MainScreen />} />
-    {/* catch all route */}
-    <Route path='*' element={<h1>Page not found!</h1>} />
-  </Route>
-))
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' >
+          <Route index element={<StartScreen />} />
+          <Route path='main' element={<MainScreen />} />
+          {/* catch all route */}
+          <Route path='*' element={<h1>Page not found!</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
